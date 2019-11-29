3441d66c80b39187a170786cc5728908efccb05c
Nizar <45555397+TAJMOUTI@users.noreply.github.com>
2019-11-29 11:08:41 +0100

    Add files via upload
    That's the button option of  the general file

---
+++ myway.java
@@ -0,0 +1,50 @@
+JButton btnCampusE = new JButton("");
+btnCampusE.setMargin(new Insets(0, 0, 0, 0));
+btnCampusE.setBounds(977, 72, 117, 83);
+contentPane.add(btnCampusE);
+ImageIcon but1 = new ImageIcon("/Users/sethmigan/Desktop/Button1.png");
+Image imgbut1 =  but1.getImage();
+Image newImgbut1 = imgbut1.getScaledInstance(btnCampusE.getWidth(), btnCampusE.getHeight(), Image.SCALE_SMOOTH);
+ImageIcon imagebut1 = new ImageIcon(newImgbut1);
+btnCampusE.setIcon(imagebut1);
+
+JButton btnNewButton = new JButton("");
+btnNewButton.setMargin(new Insets(0, 0, 0, 0));
+btnNewButton.setBounds(977, 167, 117, 83);
+contentPane.add(btnNewButton);
+ImageIcon but2 = new ImageIcon("/Users/sethmigan/Desktop/Button2.png");
+Image imgbut2 =  but2.getImage();
+Image newImgbut2 = imgbut2.getScaledInstance(btnNewButton.getWidth(), btnNewButton.getHeight(), Image.SCALE_SMOOTH);
+ImageIcon imagebut2 = new ImageIcon(newImgbut2);
+btnNewButton.setIcon(imagebut2);
+
+JButton btnCampusE_1 = new JButton("");
+btnCampusE_1.setMargin(new Insets(0, 0, 0, 0));
+btnCampusE_1.setBounds(977, 262, 117, 83);
+contentPane.add(btnCampusE_1);
+ImageIcon but3 = new ImageIcon("/Users/sethmigan/Desktop/Button3.png");
+Image imgbut3 =  but3.getImage();
+Image newImgbut3 = imgbut3.getScaledInstance(btnCampusE_1.getWidth(), btnCampusE_1.getHeight(), Image.SCALE_SMOOTH);
+ImageIcon imagebut3 = new ImageIcon(newImgbut3);
+btnCampusE_1.setIcon(imagebut3);
+
+
+JButton btnCampusE_2 = new JButton("");
+btnCampusE_2.setMargin(new Insets(0, 0, 0, 0));
+btnCampusE_2.setBounds(977, 358, 117, 83);
+contentPane.add(btnCampusE_2);
+ImageIcon but4 = new ImageIcon("/Users/sethmigan/Desktop/Button4.png");
+Image imgbut4 =  but4.getImage();
+Image newImgbut4 = imgbut4.getScaledInstance(btnCampusE_2.getWidth(), btnCampusE_2.getHeight(), Image.SCALE_SMOOTH);
+ImageIcon imagebut4 = new ImageIcon(newImgbut4);
+btnCampusE_2.setIcon(imagebut4);
+
+JButton btnCampusE_3 = new JButton("");
+btnCampusE_3.setMargin(new Insets(0, 0, 0, 0));
+btnCampusE_3.setBounds(977, 459, 117, 83);
+contentPane.add(btnCampusE_3);
+ImageIcon but5 = new ImageIcon("/Users/sethmigan/Desktop/Button5.png");
+Image imgbut5 =  but5.getImage();
+Image newImgbut5 = imgbut5.getScaledInstance(btnCampusE_3.getWidth(), btnCampusE_3.getHeight(), Image.SCALE_SMOOTH);
+ImageIcon imagebut5 = new ImageIcon(newImgbut5);
+btnCampusE_3.setIcon(imagebut5);
